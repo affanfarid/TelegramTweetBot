ed8ffd17b126cf30b4042df067b63bc65fb866de
affanfarid <affanfarid29@gmail.com>
2020-11-17 18:06:48 -0600

    initial commit

---
+++ server.py
@@ -0,0 +1,27 @@
+#from bot import telegram_TweetBot
+from bot import telegram_TweetBot
+
+
+bot = telegram_TweetBot("config.cfg")
+
+
+def make_reply(msg):
+    if msg is not None:
+        reply = "Okay"
+    return reply
+
+update_id = None
+while True:
+    print("...")
+    updates = bot.get_updates(offset=update_id)
+    updates = updates["result"]
+
+    if updates:
+        for item in updates:
+            try:
+                update_id = item["message"]["text"]
+            except:
+                message = None
+            from_ = item["message"]["from"]["id"]
+            reply = make_reply(message)
+            bot.send_message(reply,from_)
